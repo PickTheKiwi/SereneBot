5f363d8d85f57ff3fa17d6af05cc060240e8a412
PickTheKiwi <pickthekiwi@proton.me>
2024-07-24 11:37:10 +1200

    Add main file

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+import disnake
+from disnake.ext import commands
+
+
+def start_bot():
+    pass
+
+
+if __name__ == "__main__":
+    back_setup()
+    start_bot()
+else:
+    print("This file is not meant to be imported!")
+    exit(-1)
