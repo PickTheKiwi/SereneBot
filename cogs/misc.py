0cafd783a932898df7a8a3cd46fdf642cf00f772
PickTheKiwi <pickthekiwi@proton.me>
2024-07-26 19:19:01 +1200

    Misc (new): Add ping command

---
+++ cogs/misc.py
@@ -0,0 +1,19 @@
+import disnake
+from disnake.ext import commands
+
+ENABLED = True
+
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        print("Misc cog loaded")
+
+    @commands.slash_command()
+    async def ping(self, inter: disnake.ApplicationCommandInteraction):
+        await inter.response.send_message(f"{round(self.bot.latency * 1000)}ms")
+
+
+def setup(bot):
+    if ENABLED:
+        bot.add_cog(Misc(bot))
