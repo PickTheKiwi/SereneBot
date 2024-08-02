79882b2a08436943eba27ab089e4784c96994357
PickTheKiwi <pickthekiwi@proton.me>
2024-08-02 20:43:57 +1200

    Misc: Add ping latency

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,4 +1,5 @@
 import disnake
+from disnake.embeds import Embed
 from disnake.ext import commands
 
 ENABLED = True
@@ -11,7 +12,26 @@ class Misc(commands.Cog):
 
     @commands.slash_command()
     async def ping(self, interaction):
-        await interaction.response.send_message(f"{round(self.bot.latency * 1000)}ms")
+        latency = round(self.bot.latency * 1000)
+        embed_colour = 0x0
+        match latency:
+            case latency if latency < 50:
+                embed_colour = 0x0000FF
+            case latency if latency < 100:
+                embed_colour = 0x00FFFF
+            case latency if latency < 200:
+                embed_colour = 0x00FF00
+            case latency if latency < 300:
+                embed_colour = 0xFFFF00
+            case _:
+                embed_colour = 0xFF0000
+
+        embed_colour = int(embed_colour)
+        embed = Embed(
+            title="Pong!", description=f"Bot latency is {latency}ms", color=embed_colour
+        )
+
+        await interaction.response.send_message(embed=embed)
 
 
 def setup(bot):
