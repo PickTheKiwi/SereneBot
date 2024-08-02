d687667d5009dba65eb37a570ddb04772bfb4994
PickTheKiwi <pickthekiwi@proton.me>
2024-08-02 21:47:26 +1200

    User: add info command (lacking featrues)

--- cogs/user.py
+++ cogs/user.py
@@ -15,6 +15,20 @@ class User(commands.Cog):
     async def user(self, ctx):
         pass
 
+    @user.sub_command()
+    async def info(self, ctx, user: disnake.User | None = None):
+        if user is None:
+            user = ctx.author
+
+        embed = Embed(
+            description=f"Some random text to add",
+            color=888888,
+        )
+
+        embed.title = f"User stats: {user.display_name}"
+
+        await ctx.response.send_message(embed=embed)
+
 
 def setup(bot):
     if ENABLED:
